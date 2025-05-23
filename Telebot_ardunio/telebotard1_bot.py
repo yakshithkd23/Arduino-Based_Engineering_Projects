7e5c80441dafb3330731f3e4019afbb8c064274c
Yakshith K D <157264298+yakshithkd23@users.noreply.github.com>
2025-05-23 22:18:27 +0530

    Update telebotard1_bot.py

--- Telebot_ardunio/telebotard1_bot.py
+++ Telebot_ardunio/telebotard1_bot.py
@@ -4,8 +4,10 @@
      # give a new name
      #copy the api
 #create a python code and run it below same
-#then again back into a botfather then click a link for your bot link  
-
+#then again back into a botfather then click a link for your bot link
+#add a led on and led off command 
+import serial
+ser=serial.Serial('COM3',9600)
 import telepot
 import time
 
@@ -23,7 +25,12 @@ def telebotard(msg):
 
         if 'hello' in command.lower():
             bot.sendMessage(chat_id, "Hello! This is TeleBot. How can I help you?")
-
+        if 'help' in command:
+            bot.sendMessage(chat_id,"hello %s,how can i help you?"%userName)
+        if 'led on' in command:
+            ser.write(b'Y')
+        if 'led off' in command:
+            ser.write(b'N')
 # Initialize the bot with your token
 bot = telepot.Bot('7796557286:AAG9arIEMkopwgF_R5UF0vZCmgTJdCb8YGE')
 
